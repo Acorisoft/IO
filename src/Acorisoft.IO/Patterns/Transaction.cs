d6ef6cb8981a29775e2ae3a9a3c9d49221095751
Acoris <731984870@qq.com>
2020-10-30 20:31:24 +0800

    Create Transaction.cs

---
+++ src/Acorisoft.IO/Patterns/Transaction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acorisoft.Patterns
+{
+    public class Transaction : IDisposable
+    {
+        private readonly ILiteDatabase _database;
+
+        public Transaction(ILiteDatabase database)
+        {
+            _database = database;
+        }
+
+        public void Start()
+        {
+            if (!_database.BeginTrans())
+            {
+                throw new InvalidOperationException("不能开启事务");
+            }
+        }
+
+        public void Dispose()
+        {
+            if (!_database.Commit())
+            {
+                throw new InvalidOperationException("不能提交事务");
+            }
+        }
+    }
+}
