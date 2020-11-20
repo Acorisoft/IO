8a006c521c27568466db959052327d4f4c664490
Acoris <731984870@qq.com>
2020-11-20 16:44:06 +0800

    更新修改

---
+++ src/Acorisoft.IO/Patterns/InMemorySetting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acorisoft.Patterns
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class InMemorySetting
+    {
+    }
+}
